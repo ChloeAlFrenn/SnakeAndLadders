22acfc641522149870f3cb2bb437094d42433f53
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2022-12-27 16:03:52 -0500

    Initial Commit

---
+++ SnakeAndLadders/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SnakeAndLadders
+//
+//  Created by Chloe Al-Frenn on 2022-12-27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
